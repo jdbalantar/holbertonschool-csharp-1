fc890f94abbfa3d8a74be574329508ef953ab38a
F-balanta <jf.balanta@hotmail.com>
2022-04-11 14:48:04 -0500

    Print specifiers

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("percent: {0:0.00%}", percent);
+        Console.WriteLine(string.Format("Currency: ${0:0,00.00}", currency));
+	}
+}
