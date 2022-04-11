c6b18656a8b9c66534c5cd37e204b8e408025ef8
F-balanta <jf.balanta@hotmail.com>
2022-04-11 13:39:34 -0500

    Adding  character for concat string

--- 0x00-csharp-hello_world/7-concat/7-concat.cs
+++ 0x00-csharp-hello_world/7-concat/7-concat.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+
 class Program
 {
 	static void Main(string[] args)
 	{
 		string str1 = "Holberton";
 		string str2 = "School";
-		Console.WriteLine("Welcome to {0} {1}!", str1, str2);
+		str1 = str1 + " " + str2;
+		Console.WriteLine("Welcome to {0}!", str1);
 	}
 }
