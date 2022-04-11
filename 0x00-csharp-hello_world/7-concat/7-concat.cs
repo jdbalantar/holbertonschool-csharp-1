d38e71c1acf109364ce76df599ac86e7e6a763e6
F-balanta <jf.balanta@hotmail.com>
2022-04-11 12:01:21 -0500

    Update filas task

--- 0x00-csharp-hello_world/7-concat/7-concat.cs
+++ 0x00-csharp-hello_world/7-concat/7-concat.cs
@@ -1,13 +1,10 @@
-﻿
-using System;
-
+﻿using System;
 class Program
 {
 	static void Main(string[] args)
 	{
 		string str1 = "Holberton";
 		string str2 = "School";
-		str1 = str1 + " " + str2;
-		Console.WriteLine("Welcome to {0}!", str1);
+		Console.WriteLine("Welcome to {0} {1}", str1, str2);
 	}
 }
