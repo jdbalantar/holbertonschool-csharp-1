ae1a9c3e027332270ea67bfdff417dc14a99b518
F-balanta <jf.balanta@hotmail.com>
2022-04-11 13:01:28 -0500

    Fix code

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -1,12 +1,10 @@
-﻿
-using System;
+﻿using System;
 
 class Program
 {
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		Console.WriteLine("{0}{0}{0}", str);
-        Console.WriteLine("{0}", str.Substring(0,9));
+		Console.WriteLine($"{str}{str}{str}\n{str.Substring(0.9)}", str);
         }
 }
