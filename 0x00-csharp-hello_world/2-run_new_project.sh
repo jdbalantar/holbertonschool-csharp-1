9be592860f9cc1c60e10975042372b391ebdbdae
F-balanta <jf.balanta@hotmail.com>
2022-04-11 03:26:58 -0500

    Update dotnet command

--- 0x00-csharp-hello_world/2-run_new_project.sh
+++ 0x00-csharp-hello_world/2-run_new_project.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-mkdir 2-new_project;
-dotnet new console;
-dotnet build;
-dotnet run;
+dotnet new console -o 2-new_project;
+dotnet build 2-new_project;
+dotnet run 2-new_project;
