5bfae9d170eee5caf0ccf0114373a0b7e3492b8b
F-balanta <jf.balanta@hotmail.com>
2022-04-12 17:53:19 -0500

    Fizz, Buzz, FizzBuzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class _12_fizzbuzz
+{
+    static void Main(string[] args)
+    {
+        int number;
+
+        for (number = 1; number <= 100; number ++)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+                Console.Write("FizzBuzz");
+            else if (number % 3 == 0)
+                Console.Write("Fizz");
+            else if (number % 5 == 0)
+                Console.Write("Buzz");
+            else
+                Console.Write("{0}", number);
+            if (number <= 99)
+                Console.Write(" ");
+            else
+                Console.Write("\n");
+        }
+    }
+}
+
