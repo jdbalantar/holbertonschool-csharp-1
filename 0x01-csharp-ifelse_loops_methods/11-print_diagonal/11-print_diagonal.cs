2201fde170b0cbd68dc958d4517163295def025e
F-balanta <jf.balanta@hotmail.com>
2022-04-12 16:38:00 -0500

    Print diagonal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int i, j;
+
+        for (i = 0; i < length; i++)
+        {
+            for (j = 0; j < length; j++)
+            {
+                if (i > j)
+                    Console.Write(" ");
+            }
+            Console.Write("\\");
+            Console.Write("\n");
+        }
+        
+    }
+}
