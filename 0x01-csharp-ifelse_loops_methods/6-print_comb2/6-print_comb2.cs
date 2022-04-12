3be9b6f7187a890066e372ee3ea7446619d683c2
F-balanta <jf.balanta@hotmail.com>
2022-04-12 13:45:50 -0500

    Print Comb_2

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            
+            for (i = 0; i <= 9; i++)
+            {
+                for (j = 1; j <= 9; j++)
+                {
+                    if (j > i)
+                    {
+                        if (i == 8 && j == 9)
+                        {
+                            Console.Write("{0}{1}\n", i, j);
+                        }
+                        else
+                            Console.Write("{0}{1}, ", i, j);
+                    }
+                }
+            }   
+        }
+    }
+}
