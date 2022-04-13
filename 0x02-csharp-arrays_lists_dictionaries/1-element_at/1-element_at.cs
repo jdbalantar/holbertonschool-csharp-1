570ca5227f42a5f043c46c63f518318431d8f148
F-balanta <jf.balanta@hotmail.com>
2022-04-13 15:09:20 -0500

    Print element at given index

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+    {
+    public static int elementAt(int[] array, int index)
+    {
+        int i, value = 0;
+
+        if (index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return (-1);
+        }
+        for (i = 0; i < array.Length; i++)
+        {
+            array[i] = index;
+            if (array[i] == array[index])
+                value = array[index];
+        }
+        return value;
+    }
+}
+
