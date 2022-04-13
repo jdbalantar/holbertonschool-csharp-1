d26c57d5c70a3058963eee68b0602c9189d69614
F-balanta <jf.balanta@hotmail.com>
2022-04-13 15:47:00 -0500

    Replace a integer at given position of array

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        int i;
+
+        if (index < 0 || index > array.Length)
+        {
+            Console.WriteLine("Index out of range");
+            return array;
+        }
+        for (i = 0; i < array.Length; i++)
+        {
+            if (array[i] == index)
+            {
+                array[i] = n;
+            }
+        }
+        return array;
+    }
+}
+
