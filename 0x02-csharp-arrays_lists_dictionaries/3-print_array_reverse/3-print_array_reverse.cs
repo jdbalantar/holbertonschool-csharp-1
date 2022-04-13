8f0c3ab82aaf4ab22c67d1054fdf01918bc12db4
F-balanta <jf.balanta@hotmail.com>
2022-04-13 18:39:41 -0500

    All files

--- 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -5,16 +5,16 @@ class Array
     public static void Reverse(int[] array)
     {
         int i;
-        if (array.Length == 0 || array == null)
+
+        if (array != null)
         {
-            Console.Write("\n");
-        }
-        for (i = array.Length -1; i >=0; i--)    
-        {
-            Console.Write(array[i]);
-            if (i > 0)
+            for (i = array.Length - 1; i >= 0 ; i--)
+            {
+             if (i <= array.Length - 1)
+                Console.Write(array[i]);
                 Console.Write(" ");
+            }
         }
-        Console.WriteLine("\n");
+        Console.WriteLine("");
     }
 }
