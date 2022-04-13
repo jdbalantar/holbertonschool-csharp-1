494cd7f5ffc010318e004ca7d5bdd50d05b09b8a
F-balanta <jf.balanta@hotmail.com>
2022-04-13 12:56:18 -0500

    Has been update condition for print the spaces in the array ouput

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -13,16 +13,17 @@ class Array
             return null;
         }
         if (size == 0)
-        {
             Console.Write("\n");
-        }
         new_array = new int[size];
 
         for (i = 0; i < size; i++)
         {
             new_array[i] = i;
-            if (i < size)
-                Console.Write("{0} ", new_array[i]);
+            Console.Write(new_array[i]);
+            if (i < size - 1)
+                Console.Write(" ");
+                            
+            
         }
         Console.Write("\n");
         return new_array;
