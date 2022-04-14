b18c4dd9b968831aab7e09ffd2399a1c160c0f9d
F-balanta <jf.balanta@hotmail.com>
2022-04-13 20:38:00 -0500

    Elemento of list divisible by 2.

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> NewList = new List<bool>();
+        foreach(int elem in myList)
+        {
+            if (elem % 2 == 0)
+            {
+                NewList.Add(true);
+            }
+            else
+                NewList.Add(false);
+        }
+        return NewList;
+    }
+}
