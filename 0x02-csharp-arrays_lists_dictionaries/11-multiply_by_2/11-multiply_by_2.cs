6878442667f1946f20a17f15718fb77bd8fa3dc6
F-balanta <jf.balanta@hotmail.com>
2022-04-13 23:35:39 -0500

    Multiply by 2 the value of the dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> MynewDict = new Dictionary<string, int>();
+
+        if (myDict != null)
+        {
+            foreach (KeyValuePair<string, int> elelemts in myDict)
+            {
+                MynewDict.Add(elelemts.Key, elelemts.Value * 2);
+            }
+        }
+        return MynewDict;
+
+    }
+}
