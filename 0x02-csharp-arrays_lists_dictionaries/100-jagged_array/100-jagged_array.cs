48835f01bf31fd4b5a790152ee8bc79e040a09c9
F-balanta <jf.balanta@hotmail.com>
2022-04-13 23:56:22 -0500

    Jadgged array

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {	int[][] jdarray = 
+        {
+            new int[] { 0, 1, 2, 3 },
+            new int[] { 0, 1, 2, 3, 4, 5, 6 },
+            new int[] { 0, 1 }
+        };
+                
+		for (int i = 0; i < jdarray.Length; i++)
+		{
+			for (int j = 0; j < jdarray[i].Length; j++)
+			{
+				Console.Write("{0}{1}", jdarray[i][j], j == (jdarray[i].Length - 1) ? "" : " ");
+                
+			}
+			Console.WriteLine();
+		}
+        }
+    }
+}
