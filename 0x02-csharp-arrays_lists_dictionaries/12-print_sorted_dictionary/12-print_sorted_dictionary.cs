7d8cf3d9701d09b10479b9e9ce5e04ac90fe7ae8
F-balanta <jf.balanta@hotmail.com>
2022-04-13 23:46:29 -0500

    Sorted

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> MyList = new List<string>();
+        foreach (KeyValuePair<string, string> pair in myDict)
+            MyList.Add(pair.Key.ToString() + ": " + pair.Value.ToString());
+        MyList.Sort();
+        foreach (string valueDic in MyList)
+            System.Console.WriteLine(valueDic);
+    }
+}
