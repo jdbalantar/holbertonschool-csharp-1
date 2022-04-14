481f546ebc5988dc8b0abeab2c8f50ce3823f2c1
F-balanta <jf.balanta@hotmail.com>
2022-04-13 19:14:59 -0500

    Adding integers into a list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        int i;
+
+        if (size < 0)
+        {
+            Console.Write("Size cannot be negative");
+            return null;
+        }
+        List<int> MyList = new List<int>();
+        for(i = 0; i < size; i++)
+        {
+            MyList.Add(i);
+            Console.Write(MyList[i]);
+            if (i < size - 1)
+                Console.Write(" "); 
+        }
+        Console.Write("\n");
+        return MyList;
+    }
+}
