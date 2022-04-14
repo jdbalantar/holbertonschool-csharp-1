f0fc338308e1aeb905ee1078f29e2e9f5177cc08
F-balanta <jf.balanta@hotmail.com>
2022-04-13 20:11:30 -0500

    Biggest integer of a list

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -6,9 +6,10 @@ class List
 {
     public static int MaxInteger(List<int> myList)
     {
-        if (myList == null)
+        if (myList.Count == 0)
         {
             Console.WriteLine("List is empty");
+            return (-1);
         }
         return myList.Max();
     }
