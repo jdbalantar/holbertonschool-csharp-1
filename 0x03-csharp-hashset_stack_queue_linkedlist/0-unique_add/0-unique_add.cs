f4d36cc6d9e0c736c34ae8ef4bbebcfba9a160a7
F-balanta <jf.balanta@hotmail.com>
2022-04-14 17:31:04 -0500

    Sum unique integers

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int suma;
+
+        suma = 0;
+
+        HashSet<int> hashset = new HashSet<int>(myList);
+
+        foreach(int element in hashset)
+            suma += element;
+        return suma;
+    }
+}
