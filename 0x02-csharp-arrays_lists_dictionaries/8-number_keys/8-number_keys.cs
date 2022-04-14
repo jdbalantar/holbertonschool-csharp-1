528d31006fa97070c350045603cb57c7ddf591f6
F-balanta <jf.balanta@hotmail.com>
2022-04-13 23:05:49 -0500

    Number of keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int amount = 0;
+
+        foreach(KeyValuePair<string, string> mykeys in  myDict)
+        {
+            amount++;
+        }
+        return amount;
+    }
+}
