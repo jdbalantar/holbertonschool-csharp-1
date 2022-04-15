a8e160e0c21274702481ae77029c7ac6c40d0ed4
F-balanta <jf.balanta@hotmail.com>
2022-04-14 21:23:02 -0500

    Adding Library

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,32 +1,27 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        Console.WriteLine("Number of items: {0}", aStack.Count);
-
-        if (aStack.Count != 0)
-            Console.WriteLine("Top item: {0}", aStack.Peek());
-        else
-            Console.WriteLine("Stack is empty");
-
-        bool contains = aStack.Contains(search);
-
-        Console.WriteLine("Stack contains \"{0}\": {1}", search, contains);
-
-        Stack<string> newStack = new Stack<string>(aStack);
-
-        if (aStack.Contains(search))
+		Console.WriteLine($"Number of items: {aStack.Count}");
+		if (aStack.Count <= 0)
         {
-            foreach (var item in newStack)
-            {
-                if (aStack.Pop() == search)
-                    break;
-            }
+			Console.WriteLine("Stack is empty");
         }
-        aStack.Push(newItem);
-        return (aStack);
+		else
+			Console.WriteLine($"Top item: {aStack.Peek()}");
+		if (aStack.Contains(search))
+		{
+			Console.WriteLine($"Stack contains \"{search}\": True");
+			while (aStack.Pop() != search) ;
+		}
+		else
+        {
+			Console.WriteLine($"Stack contains \"{search}\": False");
+        }
+		aStack.Push(newItem);
+		return aStack;
     }
 }
