156d77c551e1fd3dd9652dcc035b9a00351abf21
F-balanta <jf.balanta@hotmail.com>
2022-04-14 21:00:51 -0500

    Elements different

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> nl1 = new List<int>();
+        List<int> nl2 = new List<int>();
+
+        foreach (int elemlis1 in list1)
+        {
+            foreach (int elemlis2 in list2)
+            {
+                if (elemlis1 == elemlis2)
+                    nl1.Add(elemlis2);
+            }
+        }
+        list1.AddRange(list2);
+        list1.Sort();
+
+        foreach (int x in list1)
+        {
+            if (nl1.Contains(x) == false)
+            {
+                nl2.Add(x);
+            }
+        }
+        return (nl2);
+    }
+}
