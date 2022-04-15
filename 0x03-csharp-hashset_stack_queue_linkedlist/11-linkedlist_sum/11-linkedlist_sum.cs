41e0fb15f209e23d1cbaea27eeac50e07910d826
F-balanta <jf.balanta@hotmail.com>
2022-04-14 22:06:31 -0500

    Sum all elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+        foreach(int i in myLList)
+            sum += i;
+        return sum;
+    }
+}
