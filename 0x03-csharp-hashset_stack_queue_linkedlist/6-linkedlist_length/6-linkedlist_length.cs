0e96365d2353369a333f2de0c65d46aa1b6ba308
F-balanta <jf.balanta@hotmail.com>
2022-04-14 21:32:38 -0500

    Amount elements in a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        var amount = 0;
+        foreach(int i in myLList)
+        {
+            amount = amount + 1;
+        }
+        return(amount);
+        
+    }
+}
