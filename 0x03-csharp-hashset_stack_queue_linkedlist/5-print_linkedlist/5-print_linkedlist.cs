4ed9482fbe998c253ba5af287461421cd08ccce6
F-balanta <jf.balanta@hotmail.com>
2022-04-14 21:27:35 -0500

    New node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {   
+        if (size < 0)
+        {
+            LinkedList<int> newnode = new LinkedList<int>();
+            return newnode;
+        }
+        List<int> new_list = new List<int>();
+        for (var i = 0; i < size; i++)
+        {
+            new_list.Add(i);
+            Console.WriteLine(i);
+        }
+        LinkedList<int> llist = new LinkedList<int>(new_list);
+        return llist;
+        
+    }
+}
