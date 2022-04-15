cdf38189713b2f94b90e11f7c60c2bf37a5ac7cd
F-balanta <jf.balanta@hotmail.com>
2022-04-14 22:14:23 -0500

    Delete node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> C_node = myLList.First;
+        int _count = 0;
+        while(C_node != null)
+        {
+            if(_count == index)
+                myLList.Remove(C_node);
+            _count += 1;
+            C_node = C_node.Next;
+        }
+    }
+}
