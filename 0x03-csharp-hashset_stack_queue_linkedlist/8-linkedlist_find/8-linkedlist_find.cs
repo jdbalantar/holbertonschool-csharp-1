388d89ab5aa66468862515c369721bfee2f26b7c
F-balanta <jf.balanta@hotmail.com>
2022-04-14 21:50:07 -0500

    returns the index position of the first ocurrence

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        var index = 0;
+        if(myLList.Contains(value))
+        {
+            foreach(int i in myLList)
+            {
+                if (i == value)
+                    return(index);
+                index += 1;
+            }
+        } 
+        else
+        // Return -1 if value not is in the list
+            return(-1);
+        return -1;
+    }
+}
