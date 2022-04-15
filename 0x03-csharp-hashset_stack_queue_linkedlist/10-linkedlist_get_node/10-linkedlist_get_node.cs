61a3ca79301fb39d54e5ee4a6ad7d1a8fe12fd0a
F-balanta <jf.balanta@hotmail.com>
2022-04-14 22:04:29 -0500

    returns the value of the nth node of a LinkedList

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int mcount = 0;
+        if (myLList.Count < n )
+            return 0;
+        foreach(int i in myLList)
+            {
+                if (mcount == n)
+                    return i;
+                mcount += 1;
+            }
+        return 0;
+    }
+}
