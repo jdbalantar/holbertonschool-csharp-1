72a5d3df33c4d6423f1f908e3056e7b817c926fa
F-balanta <jf.balanta@hotmail.com>
2022-04-14 22:09:02 -0500

    Current node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> C_node = myLList.First;
+        while(C_node != null)
+        {
+            if(C_node.Value < n)
+            {
+                C_node = C_node.Next;
+            }
+            else
+            {
+                myLList.AddBefore(C_node, n);
+                return myLList.First;
+            }   
+        }
+        myLList.AddLast(n);
+        return myLList.First;  
+    }
+}
