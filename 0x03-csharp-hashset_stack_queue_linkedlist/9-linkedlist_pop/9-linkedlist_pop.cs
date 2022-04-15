83717b70736e2689803a1359a167bf010639ac05
F-balanta <jf.balanta@hotmail.com>
2022-04-14 21:57:47 -0500

    Pop

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int result;
+        result = 0;
+        if (myLList.Count <= 0)
+        {
+            return 0;
+        }
+        result = myLList.First.Value;
+        myLList.RemoveFirst();
+        return result;
+    }
+}
