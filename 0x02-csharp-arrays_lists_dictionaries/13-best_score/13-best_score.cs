b6770c1ef9290bdfa42c04dd2927b98ec8507102
jdbalantar <jdbalantar@gmail.com>
2022-04-17 19:54:59 -0500

    Best Score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        KeyValuePair<string, int> Bscore;
+        
+        if (myList.Count == 0 || myList == null)
+            return ("None");
+
+        Bscore = myList.First();
+        foreach (KeyValuePair<string, int> element in myList)
+        {
+            if (element.Value > Bscore.Value)
+            {
+                Bscore = element;
+            }
+        }
+
+        return (Bscore.Key);
+    }
+}
