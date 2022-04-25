59073cff0c250daeef068b88fc440597756bd646
F-balanta <jf.balanta@hotmail.com>
2022-04-25 12:47:58 -0500

    Handle Exceptions Try/Catch/Finally

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int result = 0;
+        try
+        {
+            result = (a / b);
+            
+            
+        }
+        catch (System.Exception)
+        {
+             System.Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {result}");
+        }
+    }
+}
+
