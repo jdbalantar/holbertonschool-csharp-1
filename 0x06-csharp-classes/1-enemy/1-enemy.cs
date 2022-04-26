f6ba562ba017db8a85b77e6996b26f3166acaa45
F-balanta <jf.balanta@hotmail.com>
2022-04-26 18:48:10 -0500

    Don´t Compile

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary> Class Zombie </summary>
+    public class Zombies
+    {
+        /// <summary> health should be an int and have no value </summary>
+        public int health;
+        /// <summary> Construct Method of Zombie class</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
