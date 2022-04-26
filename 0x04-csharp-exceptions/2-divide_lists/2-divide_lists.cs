8875767ae7f112a19e1c8a9410fe52166020bafa
F-balanta <jf.balanta@hotmail.com>
2022-04-25 21:24:52 -0500

    Update task

--- 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -7,23 +7,22 @@ class List
     {
         List<int> result = new List<int>();
 
-        try
+        for (int i = 0; i < listLength; i++)
         {
-            for (int i = 0; i < listLength; i++)
+            try
             {
                 result.Add(list1[i] / list2[i]);
             }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                result.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
         }
-        catch(DivideByZeroException)
-        {
-            System.Console.WriteLine("Cannot divide by zero");
-            result.Add(0);
-        }
-        catch(ArgumentOutOfRangeException)
-        {
-            if (list1.Count < list2.Count)
-                System.Console.WriteLine("Out of range");
-        }
-        return result;
+        return (result);
     }
 }
