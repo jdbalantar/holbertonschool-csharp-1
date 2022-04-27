df535e5dddb88210c6d7446b556012e55e2e7fc8
F-balanta <jf.balanta@hotmail.com>
2022-04-26 19:09:21 -0500

    New constructor that modify the health value of zombie class

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Enemies
+{
+    /// <summary>  Class Zombie </summary>
+    public class Zombie 
+    {
+        /// <summary> should be an int and have no value </summary>
+        public int health;
+        /// <summary> Method Constructor that inicialize health value</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary> Constructor that update value of health </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+    }
+}
