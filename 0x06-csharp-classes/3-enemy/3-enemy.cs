5fade8a0c590910d4d0c2508c1137aedbec49fb6
F-balanta <jf.balanta@hotmail.com>
2022-04-26 19:24:12 -0500

    Private field health

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Enemies
+{
+    /// <summary>  Class Zombie </summary>
+    public class Zombie 
+    {
+        /// <summary> Private Field health should be an int and have no value </summary>
+        private int health;
+        /// <summary> Method Constructor that inicialize health value</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary> Constructor that update value of health </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+        /// <summary> Method that return the value of health of the Zombie object </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
